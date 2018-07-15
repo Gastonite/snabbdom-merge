f1df586793e20486430e87b5368c1f81c243ca0d
Clément Pasquier <clement@n00sphere.net>
2018-07-15 05:34:05 +0200

    merge selectors

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ var concat = require('ramda/src/concat')
 var mergeWith = require('ramda/src/mergeWith')
 
 // Chain multiple event handlers or hook functions together 
-var chainFuncs = curryN(4, function(data1, data2, result, key) {
+var chainFuncs = curryN(4, function (data1, data2, result, key) {
   data1[key] = data1[key] || {}
   data2[key] = data2[key] || {}
-  var chainHandlers = function(fn1, fn2) {
-    return function() {
+  var chainHandlers = function (fn1, fn2) {
+    return function () {
       fn1.apply(null, arguments)
       fn2.apply(null, arguments)
     }
@@ -23,22 +23,47 @@ var chainFuncs = curryN(4, function(data1, data2, result, key) {
 })
 
 // Merge the nested objects referenced by 'key' into 'result'
-var mergeKey = curryN(4, function(data1, data2, result, key) {
+var mergeKey = curryN(4, function (data1, data2, result, key) {
   var merged = mergeObj(data1[key], data2[key])
   return assoc(key, merged, result)
 })
 
+var parseSelector = function (selector) {
+
+  var matches = selector.match(/\#[\w-]+/, '')
+  var id = matches && matches[0] || ''
+  var selector = (!id ? selector : selector.replace(id, '')).split('.')
+
+  return {
+    tag: selector[0],
+    classes: selector.slice(1),
+    id,
+    selector
+  }
+}
 // Merge some data properties, favoring vnode2
 // Chain all hook and eventlistener functions together
 // Concat selectors together
-var merge = curryN(2, function(vnode1, vnode2) {
+var merge = curryN(2, function (vnode1, vnode2) {
   var toMerge = ['props', 'class', 'style', 'attrs', 'dataset']
   var merged = reduce(mergeKey(vnode1.data, vnode2.data), {}, toMerge)
   var toChain = ['on', 'hook']
   var chained = reduce(chainFuncs(vnode1.data, vnode2.data), merged, toChain)
   var data = compose(mergeObj(vnode1.data), mergeObj(vnode2.data))(chained)
   var children = concat(vnode1.children || [], vnode2.children || [])
-  return h(vnode2.sel, data, children)
+
+  var vnode1Sel = parseSelector(vnode1.sel)
+  var vnode2Sel = parseSelector(vnode2.sel)
+
+  var classes = ['']
+    .concat(vnode1Sel.classes)
+    .concat(vnode2Sel.classes)
+
+  return h(
+    vnode2Sel.tag + vnode2Sel.id + classes.join('.'),
+    data,
+    children
+  )
 })
 
 module.exports = merge
