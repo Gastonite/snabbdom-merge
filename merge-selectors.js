972b44640f4d437ca15bea29d153941e35fe8000
Clément Pasquier <clement.pasquier@sii.fr>
2018-11-02 23:34:03 +0100

    update mergeSelector: fallback to selector1 if selector2 is not present

--- merge-selectors.js
+++ merge-selectors.js
@@ -16,17 +16,21 @@ var parseSelector = function (selector) {
 /**
  * Combine two CSS selectors into one
  */
-var mergeSelectors = function (selector1, selector2) {
+var mergeSelectors = function (input1, input2) {
 
-  var vnode1Sel = parseSelector(selector1)
-  var vnode2Sel = parseSelector(selector2)
+  var selector1 = parseSelector(input1)
+  var selector2 = parseSelector(input2)
+
+  console.log('mergeSelectors()', {
+    selector1,
+    selector2
+  })
 
   var classes = ['']
-    .concat(vnode1Sel.classes)
-    .concat(vnode2Sel.classes)
+    .concat(selector1.classes)
+    .concat(selector2.classes)
 
-  return vnode2Sel.tag + vnode2Sel.id + classes.join('.')
+  return (selector2.tag || selector1.tag) + selector2.id + classes.join('.')
 }
-
 
 module.exports = mergeSelectors
