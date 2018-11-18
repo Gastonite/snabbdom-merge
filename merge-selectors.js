2b4900d0e557fbdcaeabc45859ebfd780661f049
Clément Pasquier <clement@n00sphere>
2018-11-18 19:51:59 +0100

    parseSelector argument defaults to empty string

--- merge-selectors.js
+++ merge-selectors.js
@@ -1,6 +1,9 @@
 
 var parseSelector = function (selector) {
 
+  if (typeof selector !== 'string')
+    selector = ''
+    
   var matches = selector.match(/\#[\w-]+/, '')
   var id = matches && matches[0] || ''
   var selector = (!id ? selector : selector.replace(id, '')).split('.')
@@ -21,15 +24,11 @@ var mergeSelectors = function (input1, input2) {
   var selector1 = parseSelector(input1)
   var selector2 = parseSelector(input2)
 
-  console.log('mergeSelectors()', {
-    selector1,
-    selector2
-  })
 
   var classes = ['']
     .concat(selector1.classes)
     .concat(selector2.classes)
-
+    
   return (selector2.tag || selector1.tag) + selector2.id + classes.join('.')
 }
 
